e63e4c4625ca091729358092678cf0ce532da265
acterhd <acterhd@gmail.com>
2016-03-19 00:18:06 +0800

    cubemaps

--- ogl_reflections_classed/ogl_reflections/main.cpp
+++ ogl_reflections_classed/ogl_reflections/main.cpp
@@ -136,6 +136,8 @@ private:
 	GLuint vbo;
 	GLuint ebo;
 
+	GLuint cubeTex;
+
 public:
 	RObject() {
 		init();
@@ -218,6 +220,10 @@ private:
 	}
 
 public:
+	void includeCubemap(GLuint cube) {
+		cubeTex = cube;
+	}
+
 	void camera(glm::vec3 eye, glm::vec3 view) {
 		glUseProgram(cameraProgram);
 		
@@ -243,6 +249,10 @@ public:
 	void close() {
 		glUseProgram(closeProgram);
 		bind();
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTex);
+		glUniform1i(glGetUniformLocation(closeProgram, "cubeTex"), 0);
 
 		glUniform2f(glGetUniformLocation(closeProgram, "sceneRes"), width, height);
 		glDispatchCompute(tiled(width, 16) / 16, tiled(height, 16) / 16, 1);
@@ -787,16 +797,16 @@ public:
 	}
 
 	void leftRight(glm::vec3 &ca, glm::vec3 &vi, float diff) {
-		ca.x -= diff / 1000.0f;
-		vi.x -= diff / 1000.0f;
+		ca.x -= diff / 500.0f;
+		vi.x -= diff / 500.0f;
 	}
 	void topBottom(glm::vec3 &ca, glm::vec3 &vi, float diff) {
-		ca.y += diff / 1000.0f;
-		vi.y += diff / 1000.0f;
+		ca.y += diff / 500.0f;
+		vi.y += diff / 500.0f;
 	}
 	void forwardBackward(glm::vec3 &ca, glm::vec3 &vi, float diff) {
-		ca.z -= diff / 1000.0f;
-		vi.z -= diff / 1000.0f;
+		ca.z -= diff / 500.0f;
+		vi.z -= diff / 500.0f;
 	}
 	void rotateY(glm::vec3 &vi, float diff) {
 		vi = glm::rotateX(vi, -diff / 5000000.0f);
@@ -806,9 +816,43 @@ public:
 	}
 };
 
+std::string cubefaces[6] = {
+	"./cubemap/c0.jpg",
+	"./cubemap/c1.jpg",
+	"./cubemap/c2.jpg",
+	"./cubemap/c3.jpg",
+	"./cubemap/c4.jpg",
+	"./cubemap/c5.jpg"
+};
 
-
-
+GLuint initCubeMap()
+{
+	GLbyte *pBytes;
+	GLint eFormat, iComponents;
+	GLint width, height;
+	GLuint tex;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, tex);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	for (int i = 0; i<6; i++)
+	{
+		sf::Image img_data;
+		std::string tex = cubefaces[i];
+		if (!img_data.loadFromFile(tex))
+		{
+			std::cout << "Could not load '" << tex << "'" << std::endl;
+			//return false;
+		}
+		if (tex != "") {
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA, img_data.getSize().x, img_data.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data.getPixelsPtr());
+		}
+	}
+	return tex;
+}
 
 
 int main()
@@ -821,9 +865,7 @@ int main()
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	
-
-
+	GLuint cubeTex = initCubeMap();
 
 	std::string inputfile = "sponza.obj";
 	std::vector<tinyobj::shape_t> shapes;
@@ -859,6 +901,7 @@ int main()
 	sponza.buildOctree();
 
 	RObject rays;
+	rays.includeCubemap(cubeTex);
 	clock_t t = clock();
 
 	Camera cam;
@@ -892,8 +935,8 @@ int main()
 
 		cam.work(c);
 
-		sponza.calcMinmax();
-		sponza.buildOctree();
+		//sponza.calcMinmax();
+		//sponza.buildOctree();
 		rays.camera(cam.eye, cam.view);
 		for (int i = 0;i < 5;i++) {
 			rays.begin();
