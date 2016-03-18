e82e9296a9917b5871f0d8df70ed896e1d65b538
acterhd <acterhd@gmail.com>
2016-03-18 19:05:18 +0800

    camera

--- ogl_reflections_classed/ogl_reflections/main.cpp
+++ ogl_reflections_classed/ogl_reflections/main.cpp
@@ -223,7 +223,7 @@ public:
 		
 		bind(); 
 
-		glUniformMatrix4fv(glGetUniformLocation(cameraProgram, "projInv"), 1, false, glm::value_ptr(glm::inverse(glm::perspective(3.14f / 3.0f, float(width) / float(height), 0.01f, 1000.0f))));
+		glUniformMatrix4fv(glGetUniformLocation(cameraProgram, "projInv"), 1, false, glm::value_ptr(glm::inverse(glm::perspective(3.14f / 3.0f, float(width) / float(height), 0.1f, 10000.0f))));
 		glUniformMatrix4fv(glGetUniformLocation(cameraProgram, "camInv"), 1, false, glm::value_ptr(glm::inverse(glm::lookAt(eye, view, glm::vec3(0.0, 1.0, 0.0)))));
 		glUniform2f(glGetUniformLocation(cameraProgram, "sceneRes"), width, height);
 
@@ -723,7 +723,88 @@ public:
 };
 
 
+class Camera {
+public:
+	glm::vec3 eye = glm::vec3(1.0, 100.0, 1.0);
+	glm::vec3 view = glm::vec3(0.0, 100.0, 0.0);
 
+	sf::Vector2i mposition;
+
+	glm::mat4 project() {
+		return glm::lookAt(eye, view, glm::vec3(0.0, 1.0, 0.0));
+	}
+
+	void work(float diff) {
+		glm::mat4 viewm = project();
+		glm::mat4 unviewm = glm::inverse(viewm);
+		glm::vec3 ca = glm::vec3(0.0f);
+		glm::vec3 vi = (viewm * glm::vec4(view, 1.0)).xyz;
+
+		sf::Vector2i position = sf::Mouse::getPosition();
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			sf::Vector2i mpos = position - mposition;
+			float diffX = mpos.x * diff;
+			float diffY = mpos.y * diff;
+			this->rotateX(vi, diffX);
+			this->rotateY(vi, diffY);
+		} 
+
+		mposition = position;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			forwardBackward(ca, vi, diff);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			forwardBackward(ca, vi, -diff);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			leftRight(ca, vi, diff);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			leftRight(ca, vi, -diff);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+		{
+			topBottom(ca, vi, diff);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+		{
+			topBottom(ca, vi, -diff);
+		}
+
+		eye = (unviewm * glm::vec4(ca, 1.0)).xyz;
+		view = (unviewm * glm::vec4(vi, 1.0)).xyz;
+	}
+
+	void leftRight(glm::vec3 &ca, glm::vec3 &vi, float diff) {
+		ca.x -= diff / 1000.0f;
+		vi.x -= diff / 1000.0f;
+	}
+	void topBottom(glm::vec3 &ca, glm::vec3 &vi, float diff) {
+		ca.y += diff / 1000.0f;
+		vi.y += diff / 1000.0f;
+	}
+	void forwardBackward(glm::vec3 &ca, glm::vec3 &vi, float diff) {
+		ca.z -= diff / 1000.0f;
+		vi.z -= diff / 1000.0f;
+	}
+	void rotateY(glm::vec3 &vi, float diff) {
+		vi = glm::rotateX(vi, -diff / 5000000.0f);
+	}
+	void rotateX(glm::vec3 &vi, float diff) {
+		vi = glm::rotateY(vi, -diff / 5000000.0f);
+	}
+};
 
 
 
@@ -780,6 +861,8 @@ int main()
 	RObject rays;
 	clock_t t = clock();
 
+	Camera cam;
+
 	bool running = true;
 	while (running)
 	{
@@ -796,7 +879,7 @@ int main()
 
 		clock_t tt = clock();
 		clock_t c = tt - t;
-
+		/*
 		glm::vec3 eye = glm::vec3(1.0, 100.0, 1.0);
 		glm::vec3 view = glm::vec3(0.0, 100.0, 0.0);
 		//glm::vec3 eye = glm::vec3(2.0, 0.0, 2.0);
@@ -805,10 +888,13 @@ int main()
 		eye = rotate(eye, ((float)c) / 5000.0f, glm::vec3(0.0, 1.0, 0.0));
 		eye += view;
 		view = eye + normalize(view - eye);
+		*/
+
+		cam.work(c);
 
 		//sponza.calcMinmax();
 		//sponza.buildOctree();
-		rays.camera(eye, view);
+		rays.camera(cam.eye, cam.view);
 		for (int i = 0;i < 2;i++) {
 			rays.begin();
 			sponza.intersection(rays);
