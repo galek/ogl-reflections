edc907975816a4679b4d6557aaf8a83c2402aedb
acterhd <acterhd@gmail.com>
2016-04-02 18:43:10 +0800

    material tests

--- ogl_reflections_classed/ogl_reflections/main.cpp
+++ ogl_reflections_classed/ogl_reflections/main.cpp
@@ -1217,6 +1217,7 @@ int main()
 		
 		msponza.resize(materials.size());
 		for (int i = 0;i < msponza.size();i++) {
+			msponza[i].setTransmission(materials[i].transmittance[0]);
 			msponza[i].setReflectivity(materials[i].shininess);
 			msponza[i].setDissolve(materials[i].dissolve);
 			msponza[i].setBump(loadBump(materials[i].bump_texname));
